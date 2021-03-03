80c35bb27925714af68d9661ba96e6153ff4ce2b
Alejandro Montilla <luis.alejandro.499be@gmail.com>
2021-03-03 22:14:40 +0100

    Handle warnings

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from '../../img/GitHub-Mark-32px.png';
 
 export const Footer = () => {
 	return (
 		<footer className='footer'>
-			<a href='https://github.com/AlejoVE' target='_blank'>
-				<img src='https://user-images.githubusercontent.com/59319966/109864628-584d4e80-7c63-11eb-9f1a-4afed77758ce.png'></img>
+			<a href='https://github.com/AlejoVE' target='_blank' rel='noreferrer'>
+				<img
+					src='https://user-images.githubusercontent.com/59319966/109864628-584d4e80-7c63-11eb-9f1a-4afed77758ce.png'
+					alt='github logo'
+				></img>
 			</a>
 			<p>Alejandro Montilla ©2021</p>
 		</footer>
