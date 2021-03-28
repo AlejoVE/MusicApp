fce5183b935d5f75016055b0abe95410e4eda48a
Alejandro Montilla <luis.alejandro.499be@gmail.com>
2021-03-28 17:10:56 +0200

    Remove Spotify logos

--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -8,12 +8,15 @@ export const MainScreen = () => {
 		<>
 			<div className='main-container animate__animated animate__fadeIn'>
 				<div className='header-container'>
-					<img
+					{/* <img
 						className='header-img'
+						// src='https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Black.png'
 						src='https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Black.png'
 						alt='Spotify logo'
-					></img>
+					></img> */}
+					<h1>MusicApp</h1>
 				</div>
+				<h2 className='sub-title'>Select a category</h2>
 				<CategoryContainer />
 				<Footer />
 			</div>
--- src/components/NotSelected.js
+++ src/components/NotSelected.js
@@ -5,8 +5,9 @@ export const NotSelected = () => {
 		<div className='not-selected-container animate__animated animate__fadeIn'>
 			<h2>Please select a Playlist</h2>
 			<img
-				src='https://www.freepnglogos.com/uploads/spotify-logo-png/file-spotify-logo-png-4.png'
-				alt='Spotify logo'
+				// src='https://www.freepnglogos.com/uploads/spotify-logo-png/file-spotify-logo-png-4.png'
+				src='https://cdn.pixabay.com/photo/2017/01/31/22/58/abstract-2027962_960_720.png'
+				alt='headphones logo'
 			/>
 		</div>
 	);
